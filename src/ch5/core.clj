939e15fa1c2af93dd6f27b632bda53b708a2686d
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-05-05 17:14:10 +0200

    :3

--- src/ch5/core.clj
+++ src/ch5/core.clj
@@ -68,7 +68,36 @@
 ;; -----------------------------------------------------------------------------
 
 (defn my-assoc-in
-  [m [k & vs] v])
+  [m [k & ks] v]
+  (reduce (fn [map key]
+            (assoc {} key map))
+          (assoc m k v)
+          ks))
+
+; Uh.. oups! This is the wrong way around.
+(my-assoc-in {} [:a :b :c] 5) ; => {:c {:b {:a 5}}}
+(my-assoc-in {:v 7} [:a :b :c] 5) ; => {:c {:b {:v 7, :a 5}}} <-- That's wrong as well!
+
+(defn my-assoc-in
+  [m [k & ks] v]
+  (if (empty? ks)
+    (assoc m k v)
+    (assoc m k (my-assoc-in {} ks v))))
 
 (assoc-in {} [:a :b :c] 5) ; => {:a {:b {:c 5}}}
+(assoc-in {:v 7} [:a :b :c] 5) ; => {:v 7, :a {:b {:c 5}}}
 (my-assoc-in {} [:a :b :c] 5) ; => {:a {:b {:c 5}}}
+(my-assoc-in {:v 7} [:a :b :c] 5) ; => {:v 7, :a {:b {:c 5}}}
+
+(assoc {} :a (assoc {} :b (assoc {} :c 5)))
+
+;; -----------------------------------------------------------------------------
+
+(update-in {:a {:c 5} :b 7} [:a :c] inc) ; => {:a {:c 6}, :b 7}
+(update-in {:a {:c 5} :b 7} [:a :c] #(/ %1 %2) 2) ; => {:a {:c 5/2}, :b 7}
+
+(defn my-update-in
+  [m ks f & args]
+  (assoc-in m ks (apply f (get-in m ks) args)))
+
+(my-update-in {:a {:c 5} :b 7} [:a :c] #(/ %1 %2) 2) ; => {:a {:c 5/2}, :b 7}
