f1271b888200bfe53754c5b64e2e30131d74c5c4
Colin van Loo <colin.vanloo@edu.tbz.ch>
2023-05-18 23:07:31 +0200

    feat(ch7): awful solution to exercise 2 of ch7

--- src/ch5/core.clj
+++ src/ch5/core.clj
@@ -51,7 +51,6 @@
 ((attr :i) my-map) ; => 77
 ((attr :z) my-map) ; => nil
 
-
 ;; -----------------------------------------------------------------------------
 
 (defn my-comp
@@ -112,3 +111,83 @@
   (assoc-in m ks (apply f (get-in m ks) args)))
 
 (my-update-in {:a {:c 5} :b 7} [:a :c] #(/ %1 %2) 2) ; => {:a {:c 5/2}, :b 7}
+
+;; -----------------------------------------------------------------------------
+
+; 1 + 2 * 3 = 7
+; (+ 1 (* 2 3) = 7
+
+(def precedences
+  {nil 0
+   :+ 1
+   :- 1
+   :/ 2
+   :* 2})
+
+(def get-precedence (partial get precedences))
+
+(defn lower-precedence?
+  [op1 op2]
+  (let [op1 (if (symbol? op1) (keyword op1) op1)
+        op2 (if (symbol? op2) (keyword op2) op2)]
+    (< (get-precedence op1) (get-precedence op2))))
+
+(defn push-op
+  [stack op]
+  (update-in stack [:ops] #(conj % op)))
+
+(defn top-op
+  [stack]
+  (first (:ops stack)))
+
+(defn push-num
+  [stack num]
+  (update-in stack [:nums] #(conj % num)))
+
+(defn create-pair
+  [{nums :nums ops :ops}]
+  (let [op (first ops)
+        [num1 num2] (take 2 nums)
+        pair (list op num1 num2)]
+    {:nums (conj (drop 2 nums) pair) :ops (rest ops)}))
+
+;(eval (first (:nums (create-pair {:nums '(3 2 1) :ops (list * +)}))))
+
+(defn pre-infix
+  [infixed]
+  (reduce (fn [a e]
+            (if (number? e)
+              (push-num a e)
+              (push-op (if (lower-precedence? e (top-op a))
+                         (create-pair a)
+                         a)
+                       e)))
+          {:nums '() :ops '()}
+          infixed))
+
+(defmacro infix
+  [infixed]
+  (loop [a (pre-infix infixed)]
+    (if (empty? (:ops a))
+      (first (:nums a))
+      (recur (create-pair a)))))
+
+; iterate through input list
+;   el is a number
+;     push to number stack
+;   el is an operator
+;     el has higher or equal precedence to top of operator stack
+;       push to operator stack
+;     el has lower precedence to top of operator stack
+;       put top of op stack and top two nums into list and store list back onto number stack
+;       put operator onto op stack
+; while op stack not empty
+;   take top two numbers from number stack
+;   take top operator from operator stack
+;   add together in a list, place back on number stack
+
+(macroexpand '(infix (1 + 2 * 3))) ; => (+ (* 3 2) 1)
+(infix (1 + 2 * 3)) ; => 7
+
+(macroexpand '(infix (1 * 2 + 3))) ; => (+ 3 (* 2 1))
+(infix (1 * 2 + 3)) ; => 5
